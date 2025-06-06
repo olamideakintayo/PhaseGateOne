cbd3618ea2188b8278e9e1024476a2883b6cfdef
olamideakintayo <divineolamide977@gmail.com>
2025-06-06 14:26:35 +0100

    setting the period variables in the class

---
+++ Java/MenstrualCycle/MenstrualCycle.java
@@ -0,0 +1,79 @@
+// MenstrualCycle.java
+
+import java.util.Scanner;
+import java.time.LocalDate;
+
+public class Menstruation {
+
+	static Scanner input = new Scanner(System.in);
+	static ArrayList<Menstruation> menstrualCycle = new ArrayList<>();
+	
+	String name;
+	int previousPeriodDate;
+	int currentPeriodDate;
+	
+	
+	public Menstruation(String name, int previousPeriodDate, int currentPeriodDate) {
+	this.name = name;
+	this.previousPeriodDate = previousPeriodDate;
+	this.currentPeriodDate = currentPreiodDate;
+	}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
