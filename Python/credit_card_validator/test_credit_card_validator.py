cb4e617858dd93ef8863e4372a06494899e7d0a4
olamideakintayo <divineolamide977@gmail.com>
2025-06-09 15:00:15 +0100

    test that checks for a valid card

--- Python/credit_card_validator/test_credit_card_validator.py
+++ Python/credit_card_validator/test_credit_card_validator.py
@@ -3,8 +3,13 @@
 import unittest
 from unittest import TestCase
 
-import credit_card_validator 
+from credit_card_validator import luhn_check, credit_card_validation
 
 class TestCreditCardValidator(TestCase):
-	def setUp(self):
-		numbers.clear()
+		
+	def test_that_checks_for_a_valid_visa_card(self):
+		credit_card_number = "4539148803436467"
+		result = credit_card_validation(credit_card_number)
+		
+		
+		self.assertTrue(result["valid"])
