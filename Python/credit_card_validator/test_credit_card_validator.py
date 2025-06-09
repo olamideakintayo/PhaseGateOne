0fd72891fd74e5469bb3b11c43313fb102f45273
olamideakintayo <divineolamide977@gmail.com>
2025-06-09 14:27:24 +0100

    setUp class from unittest to clear the numbers after each test session ran

---
+++ Python/credit_card_validator/test_credit_card_validator.py
@@ -0,0 +1,10 @@
+# test_credit_card_validator.py
+
+import unittest
+from unittest import TestCase
+
+import credit_card_validator 
+
+class TestCreditCardValidator(TestCase):
+	def setUp(self):
+		numbers.clear()
