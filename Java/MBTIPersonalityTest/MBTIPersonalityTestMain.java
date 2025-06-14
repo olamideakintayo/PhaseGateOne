4515a3bf9d73cd784fda21244bee3dac704fa077
olamideakintayo <divineolamide977@gmail.com>
2025-06-14 14:52:51 +0100

    ReOrganization of files

--- Java/MBTIPersonalityTest/MBTIPersonalityTestMain.java
+++
@@ -1,7 +0,0 @@
-//MBTIPersonalityTestMain.java
-
- public class Main {
-    public static void main(String[] args) {
-        MBTIPersonalityTest.runPersonalityTest();
-    }
-}
--- JavaScript/creditCardValidator/creditCardValidatorMain.js
+++
@@ -1,1 +0,0 @@
-// creditCardValidatorMain.js
