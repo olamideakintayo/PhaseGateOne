cbaf5d3c9a416263883c82b5f9cb373e41e3b712
olamideakintayo <divineolamide977@gmail.com>
2025-06-15 19:05:38 +0100

    initial commit

---
+++ daySeven/phoneBook/Java/PhoneBook.java
@@ -0,0 +1,83 @@
+// PhoneBook.java
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PhoneBook {
+static Scanner input = new Scanner(System.in);
+    static ArrayList<PhoneBook> numbers = new ArrayList<>();
+
+	String firstName;
+	String lastName;
+	int phoneNumber;
+}
+
+	public PhoneBook(String firstName, String lastName, int phoneNumber) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.phoneNumber = phoneNumber;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
